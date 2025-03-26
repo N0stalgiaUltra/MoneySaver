cb7d8144be759da9357c1c863fc259f1d200ad43
Vinicius Tavares <viniciustavares@declink.com.br>
2025-03-26 11:30:45 -0300

    added all mock methods to view model and added unit tests to view model

--- app/src/main/java/com/example/moneysaver/viewmodel/ExpensesViewModel.kt
+++ app/src/main/java/com/example/moneysaver/viewmodel/ExpensesViewModel.kt
@@ -14,7 +14,6 @@ class ExpensesViewModel(): ViewModel() {
     //buffer
     private val mutableExpenses = mutableListOf<Expense>()
 
-    // TODO: 1 - Adicionar Mocks antes de implementar a lógica no Room
     // TODO: 2 - Adicionar Injeção de Dependência
     // TODO: 3 - Implementar a Lógica basica correta com o uso do Room
 
@@ -22,13 +21,55 @@ class ExpensesViewModel(): ViewModel() {
         _expensesList.value = emptyList()
     }
 
-    fun loadAllExpenses(){}
+    fun loadAllExpenses(){
+
+        // TODO: 1 - Recuperação tem que ocorrer com o Room
+
+        val listExpenses = listOf(
+            Expense(
+                0,
+                "Celular",
+                100.00,
+                "03/02/1998",
+                "Despesas",
+                "Celular Novo"
+            ), Expense(
+                1,
+                "Chuteira",
+                500.00,
+                "03/02/2023",
+                "Custos Simples",
+                "Chuteira nova"
+            ), Expense(
+                2,
+                "Plano de saude",
+                450.00,
+                "01/12/2005",
+                "Despesas",
+                "Plano de saude novo"
+            ),
+        )
+        _expensesList.value = listExpenses
+    }
+
     fun addExpense(expense: Expense){
-        if(!_expensesList.value!!.contains(expense)){
+        // TODO: 1 - Adicionar com UseCase
+        // TODO: 2 - Adicionar no Room
+
+        if(!mutableExpenses.contains(expense)){
             mutableExpenses.add(expense)
             _expensesList.value = mutableExpenses.toList()
         }
     }
-    fun deleteExpense(expense: Expense){}
+    fun deleteExpense(expense: Expense){
+        // TODO: 1 - Remover com UseCase
+        // TODO: 2 - Remover no Room
+        _expensesList.value = mutableExpenses.filter { it != expense }
+        if(mutableExpenses.contains(expense))
+            mutableExpenses.remove(expense)
+
+    }
+
+
 
 }
--- app/src/test/java/com/example/moneysaver/viewmodel/ExpensesViewModelTest.kt
+++ app/src/test/java/com/example/moneysaver/viewmodel/ExpensesViewModelTest.kt
@@ -32,6 +32,7 @@ class ExpensesViewModelTest {
         assert(actualList.isNullOrEmpty())
     }
 
+    //addExpenses tests
     @Test
     fun `when addExpense is called, the list should add the expense`(){
         assert(viewModel.expensesList.value.isNullOrEmpty())
@@ -46,10 +47,123 @@ class ExpensesViewModelTest {
         )
 
         viewModel.addExpense(expense)
+
         val actualList = viewModel.expensesList.value
         assertTrue(actualList!!.isNotEmpty())
         assertTrue(
             actualList.contains(expense)
         )
+    }
+
+    @Test
+    fun `addExpense should not allow to insert the same Expense twice`(){
+        val expense = Expense(
+            0,
+            "Celular",
+            100.00,
+            "03/02/1998",
+            "Despesas",
+            "Celular Novo"
+        )
+        viewModel.addExpense(expense)
+
+        val expenseCopy = Expense(
+            0,
+            "Celular",
+            100.00,
+            "03/02/1998",
+            "Despesas",
+            "Celular Novo"
+        )
+
+        viewModel.addExpense(expenseCopy)
+
+        val expenseList = viewModel.expensesList.value
+        assertTrue(expenseList!!.size == 1)
+    }
+
+    //loadAllExpenses tests - tem que ser adaptados com UseCase
+//    @Test
+//    fun `if list is empty, loadAllExpenses should return an empty list`(){
+//        viewModel.loadAllExpenses()
+//        val expenseList = viewModel.expensesList
+//        assertTrue(expenseList.value.isNullOrEmpty())
+//
+//    }
+//
+    @Test
+    fun `if list is not empty, loadAllExpenses should return the list`(){
+        viewModel.loadAllExpenses()
+        val expenseList = viewModel.expensesList
+        assertTrue(expenseList.value!!.isNotEmpty())
+    }
+
+    @Test
+    fun `if the list is empty, should not delete nothing`(){
+        val emptyExpenseList = viewModel.expensesList.value
+        assertTrue(emptyExpenseList!!.isEmpty())
+
+        viewModel.deleteExpense(getExpensesList()[0])
+
+        val expenseList = viewModel.expensesList.value
+        assertTrue(expenseList!!.isEmpty())
+    }
+
+    @Test
+    fun `if expense is not in the expense list, should not delete nothing`(){
+        val expense = Expense(
+            4,
+            "Teste",
+            100.00,
+            "03/02/1998",
+            "Teste",
+            "Teste"
+        )
+
+        viewModel.deleteExpense(expense)
+
+        //in this case, list is empty
+        val expenseList = viewModel.expensesList.value
+        assertTrue(expenseList!!.isEmpty())
+        assertTrue(!expenseList.contains(expense))
+    }
+
+    @Test
+    fun `if expense is in expense list, deleteExpenses should delete the expense`(){
+        viewModel.loadAllExpenses()
+        val expenseList = viewModel.expensesList
+        val expense = expenseList.value!![0]
+
+        viewModel.deleteExpense(expense)
+        assertTrue(!expenseList.value!!.contains(expense))
+    }
+    //Mock do loadAllExpenses
+    companion object{
+        fun getExpensesList(): List<Expense>{
+            return listOf(
+                Expense(
+                    0,
+                    "Celular",
+                    100.00,
+                    "03/02/1998",
+                    "Despesas",
+                    "Celular Novo"
+                ), Expense(
+                    1,
+                    "Chuteira",
+                    500.00,
+                    "03/02/2023",
+                    "Custos Simples",
+                    "Chuteira nova"
+                ), Expense(
+                    2,
+                    "Plano de saude",
+                    450.00,
+                    "01/12/2005",
+                    "Despesas",
+                    "Plano de saude novo"
+                ),
+            )
+        }
     }
 }
