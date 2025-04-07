d80054ae9ee11bdd40a21b3791880da58dd4200f
Vinicius Tavares <viniciustavares@declink.com.br>
2025-04-07 16:49:06 -0300

    Added the setup for the add note screen

--- app/src/main/java/com/example/moneysaver/fragments/MainScreen.kt
+++ app/src/main/java/com/example/moneysaver/fragments/MainScreen.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.moneysaver.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainScreen : Fragment() {
 
@@ -20,7 +22,15 @@ class MainScreen : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_screen, container, false)
+        val view = inflater.inflate(R.layout.fragment_main_screen, container, false)
+
+        val testBtn = view.findViewById<FloatingActionButton>(R.id.fab_add)
+        testBtn.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_mainScreen_to_addExpense)
+        }
+
+
+        return view
     }
 
 
