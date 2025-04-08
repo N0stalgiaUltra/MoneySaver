e1f6ca3c075d0651eb90e4fca4d0846d610d3d3c
Vinicius Tavares <viniciustavares@declink.com.br>
2025-04-08 15:11:29 -0300

    Added fragment and ui tests to the edit text and button

--- app/src/androidTest/java/com/example/moneysaver/AddExpenseInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/moneysaver/AddExpenseInstrumentedTest.kt
@@ -1,14 +1,22 @@
 package com.example.moneysaver
 
 import android.app.Activity
+import android.database.sqlite.SQLiteConstraintException
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.PerformException
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.moneysaver.fragments.AddExpense
+import junit.framework.Assert.assertTrue
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.fail
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,12 +27,32 @@ class AddExpenseInstrumentedTest {
 
     @Test
     fun shouldDisplayAddNameTextField(){
-
         launchFragmentInContainer<AddExpense>(
-            themeResId = R.style.Theme_MoneySaver
-        )
+            themeResId = R.style.Theme_MoneySaver)
 
         onView(withId(R.id.et_name))
             .check(matches(isDisplayed()))
     }
+
+    @Test
+    fun shouldNotAddItemIfNameIsEmpty(){
+        launchFragmentInContainer<AddExpense>(
+            themeResId = R.style.Theme_MoneySaver)
+
+        val onViewEditText = onView(withId(R.id.et_name))
+
+        onViewEditText.check(matches(isDisplayed()))
+        onViewEditText.perform(typeText(""))
+
+        try {
+            val addButton = onView(withId(R.id.btn_add_expense))
+            addButton.check(matches(isDisplayed())).perform(click())
+            fail("Expected an exception to be thrown")
+        }catch (e: PerformException){
+            assertTrue(e.cause is IllegalArgumentException)
+            assertEquals("Name cannot be empty", e.cause?.message)
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/moneysaver/fragments/AddExpense.kt
+++ app/src/main/java/com/example/moneysaver/fragments/AddExpense.kt
@@ -1,14 +1,19 @@
 package com.example.moneysaver.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.moneysaver.R
+import com.example.moneysaver.databinding.FragmentAddExpenseBinding
 
 
 class AddExpense : Fragment() {
+
+    private lateinit var binding: FragmentAddExpenseBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +25,20 @@ class AddExpense : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_expense, container, false)
+        binding = FragmentAddExpenseBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnAddExpense.setOnClickListener {
+            val nome = binding.etName.text.toString()
+
+            // TODO: Criar uma classe para validar se os edit texts estarão vazios 
+            if(nome.trim().isEmpty())
+                throw IllegalArgumentException("Name cannot be empty")
+        }
+    }
 }
